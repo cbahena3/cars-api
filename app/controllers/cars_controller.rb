3cfff90e7bbedc48c778b7c2c8997fd4a874c297
Cristian B <cris.bahena77@gmail.com>
2023-12-07 22:43:40 -0600

    added update and route but get nil error for update test

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -18,4 +18,15 @@ class CarsController < ApplicationController
     @car = Car.find_by(id: params[:id])
     render :show
   end
+
+  def update
+    @car = Car.find_by(id: params[:id])
+    @car.update(
+      make: params[:make] || @car.make,
+      model: params[:model] || @car.model,
+      color: params[:color] || @car.color,
+      year: params[:year] || @car.year
+    )
+    render :show
+  end
 end
--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -24,4 +24,11 @@ class CarsControllerTest < ActionDispatch::IntegrationTest
     assert_equal ["id", "make", "model", "color", "year", "created_at", "updated_at"], data.keys
   end
 
+  test "update" do
+    car = Car.first
+    patch "/cars/#{car.id}.json", params: { name: "Updated name" }
+    assert_response 200
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["name"]
+  end
 end
